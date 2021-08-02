caa08fb2b969d358684aa61ced83aef003083611
Asish Das <asishdas1985@gmmail.com>
2021-08-02 14:37:04 +0530

    firebase cloud messaging and prod api integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
 /* eslint-disable @typescript-eslint/member-ordering */
 /* eslint-disable @typescript-eslint/naming-convention */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { UserService } from './pages/login/user.service';
 import { SwPush } from '@angular/service-worker';
 import { SubscriptionDetails } from './models/subscription-details';
 import { take } from 'rxjs/operators';
+import { Capacitor } from '@capacitor/core';
+import {
+  ActionPerformed,
+  PushNotifications,
+  PushNotificationSchema,
+  Token,
+} from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private userService: UserService,
     private navCtrl: NavController,
@@ -24,11 +31,57 @@ export class AppComponent {
   VAPID_PUBLIC_KEY =
     'BHDNHLflG0CSxnKyC71y-9ZhJSn4Gfht1WSpvFLFtnIJ9BTdcSQ7e2F_wT1Dx3EW0MqhUdhoylcSJ69VGFMizmc';
 
+  initPushNotifications() {
+    PushNotifications.requestPermissions().then((result) => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, token: ' + token.value);
+    });
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
+  }
+
+  ngOnInit() {
+    const isPushNotificationsAvailable =
+      Capacitor.isPluginAvailable('PushNotifications');
+
+    if (isPushNotificationsAvailable) {
+      console.log('Enabling push notification...')
+      this.initPushNotifications();
+    }
+  }
+
   logout() {
     this.userService.logout();
     this.navCtrl.pop();
     this.router.navigate(['/', 'login']);
-
   }
 
   subscribeToNotifications() {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiBaseUrl: 'https://oxyplus-api.mybluemix.net',
   googleMapsAPIKey: 'AIzaSyBUUOyUvbNEznDVaUbNlnVVOlXpBaUnFoI',
   mapBoxToken: 'pk.eyJ1IjoiYXNpc2hkYXMxOTg1IiwiYSI6ImNrcmRlaGU2dzRlMXIycXM2NHgwOWZseG8ifQ.9zmTWE8taNrzRwlpwSE59g'
 };
